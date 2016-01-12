8275e3ce0783de446b32a2485b682514bc8b3e6e
Alissa Paluska and Jacob Steinberg <alissa.paluska@gmail.com>
2016-01-12 09:18:31 -0800

    add webpage cat vs dog

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("button#cat-btn").click(function() {
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#cat-btn-img").click(function() {
+    $("div#cat-img").prepend('<img src="img/cat.jpg">');
+    $("div#cat-img").children("img").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#dog-btn").click(function() {
+    $("ul#dog").prepend("<li>Woof!</li>");
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#dog-btn-img").click(function() {
+    $("div#dog-img").prepend('<img src="img/dog.jpg">');
+    $("div#dog-img").children("img").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
